5fc75e7cd9f384add04a2c6670b7058165d1813e
Elisei <markizdelakarabas@gmail.com>
2026-01-09 11:51:52 +0500

    Delete core directory

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls  import path
-from .views import show, sorted_up, sorted_down
-
-urlpatterns = [
-    path("", show, name="home"),
-    path("sorted_up/", sorted_up),
-    path("sorted_down/", sorted_down)
-]
-
